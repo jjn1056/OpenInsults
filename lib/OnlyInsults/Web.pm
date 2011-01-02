c10cc9240e930a2367517364b8727e0ddae0bd96
John Napiorkowski <jjn1056@yahoo.com>
2011-01-02 18:33:05 -0500

    initial commit

---
+++ lib/OnlyInsults/Web.pm
@@ -0,0 +1,38 @@
+package OnlyInsults::Web;
+
+use 5.008008;
+use strict;
+use warnings FATAL =>'all';
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 NAME
+
+OnlyInsults::Web - My Brand New Module
+
+=head1 SYNOPSIS
+
+  use OnlyInsults::Web;
+
+=head1 DESCRIPTION
+
+OnlyInsults::Web is ...
+
+=head1 AUTHOR
+
+John Napiorkowski L<email:jjnapiork@cpan.org>
+
+=head1 SEE ALSO
+
+    TBD
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2011, John Napiorkowski
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
